644398cc3bf38e63b1e343d7ef9492f73a41e0ba
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-04-26 15:50:25 -0700

    commit

--- utils/snmp.py
+++ utils/snmp.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
-# Imports
 import argparse
 from easysnmp import Session
-import math
 
 '''
 useful OIDs
@@ -11,21 +9,8 @@ uptime = 1.3.6.1.2.1.1.3.0
 cpu temp = 1.3.6.1.2.1.25.1.8
 '''
 
-def time_seconds(value):
-    '''Returns UNIX time in human readable format'''
-    value = float(value)
-    hours = math.floor(value / 3600)
-    days = math.floor(hours / 24)
-    remainder = value - (hours * 3600)
-    remainder_hours = hours - (days * 24)
-    minutes = math.floor(remainder / 60)
-    seconds = remainder - int(minutes * 60)
-    feedback = f'Device has been online for {days} days, {remainder_hours} hours & {minutes} minutes & {seconds} seconds '
-    return feedback
-
-
 parser = argparse.ArgumentParser(
-    description="extracts equipment status variable via EasySNMP and returns state of health dependent on arguments given"
+    description="makes snmp requests to host with given OID"
 )
 parser.add_argument("-H", "--host", required=True, help="host to connect to")
 parser.add_argument("-p", "--port", required=True, help="port to connect to")
@@ -45,7 +30,7 @@ session = Session(hostname=f"{HOST}:{PORT}", community=COMMUNITY, version=2)
 try:
     value = session.get(SNMP_OID).value
     value = float(value)
-    print(value)
+    print(value/6000)
 except:
     print("CRITICAL: Failed to connect to device")
     exit(2)
