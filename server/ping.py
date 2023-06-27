3de1d3fd4b5c21157f7335867c410b2533c0a6d1
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-06-26 18:06:26 -0700

    having coctails w lucy and parents

--- server/ping.py
+++ server/ping.py
@@ -4,6 +4,7 @@ import pandas as pd
 # print(type(ping("192.168.86.1", verbose=True)))
 df = pd.read_csv("server/nodes.csv")
 
+
 for ip in df["ip"]:
     try:
         ping_node = ping(ip)
@@ -12,8 +13,6 @@ for ip in df["ip"]:
             print(f"Average rrt: {ping_node.rtt_avg_ms}")
         else:
             print("cant ping server")
-            print(f"Packets lost: {ping_node.stats_packets_lost}")
     except OSError as e:
         print(e)
         print("cant ping server")
-        print(f"Packets lost: {ping_node.stats_packets_lost}")
