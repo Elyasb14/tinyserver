538609942bad98f084f4b3d6c50f1f1ef0e8ded2
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-07-04 18:04:47 -0700

    good sftp changes

--- server/monitor.py
+++ server/monitor.py
@@ -5,6 +5,9 @@ class Monitor:
         self.ip = ip
 
     def ping_ip(self) -> str:
+        '''
+        pings given ip and logs in logs/ping_log.txt
+        '''
         try:
             ping_node = ping(self.ip)
             if ping_node.success():
@@ -13,12 +16,33 @@ class Monitor:
                 return f"can't ping {self.ip}"
         except OSError as e:
             return f"cant ping {self.ip}. Error: {e}"
+    
+    def snmp_ip(self, oid: str) -> str:
+        '''
+        returns given oid value from snmp request
+        '''
+        from easysnmp import Session
+        session = Session(hostname=f"{self.ip}:161", community='public', version=2)
+        if oid == '1.3.6.1.2.1.25.1.8':
+            value = f'{float(session.get(oid).value) / 1000} deg C'
+        elif oid == '1.3.6.1.2.1.1.3.0':
+            value = f"{(float(session.get(oid).value) / 100) / 60} minutes"
+        else:
+            value = session.get(oid).value
+        return value
+
 
 
 if __name__ == '__main__':
     import pandas as pd
     df = pd.read_csv('server/nodes.csv')
+    oids = {'sys info': '1.3.6.1.2.1.1.1.0',
+            'cpu temp, deg c': '1.3.6.1.2.1.25.1.8',
+            'uptime': '1.3.6.1.2.1.1.3.0'
+            }
     for ip in df['ip']:
         server = Monitor(ip)
+        for oid in oids.values():
+            print(server.snmp_ip(oid))
         with open("/Users/ebianchi/programs/tinyserver/server/logs/ping_log.txt", 'a') as file:
             file.write(f'{server.ping_ip()} \n')
--- server/sftp.py
+++ server/sftp.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 from datetime import datetime
 
-def sftp_ip(ip: str) -> str:
+def sftp_ip(ip: str, dev_name: str) -> str:
         ssh_client = SSHClient()
         ssh_client.set_missing_host_key_policy(AutoAddPolicy())
         ssh_client.connect(hostname=ip, username='ebianchi', password='Skiing#2')
@@ -14,7 +14,7 @@ def sftp_ip(ip: str) -> str:
             if file == ".config":
                 continue
             ftp_client.get(remotepath=f'/home/ebianchi/tinyweb/tinyweather/data/{file}', 
-                        localpath=f'/Users/{os.getlogin()}/programs/tinyserver/data/{file}'
+                        localpath=f'/Users/{os.getlogin()}/programs/tinyserver/data/{dev_name}/{file}'
                         )
         ftp_client.close()
         return f"data retrieval successful from {ip}"
@@ -22,6 +22,7 @@ def sftp_ip(ip: str) -> str:
 
 if __name__ == "__main__":
     df = pd.read_csv('server/nodes.csv')
-    with open(f"/Users/{os.getlogin()}/programs/tinyserver/server/logs/sftp_log.txt", 'a') as file:
-        file.write(f"{sftp_ip('10.1.1.12')} at {datetime.now()}\n")
+    for ip, dev_name in df.itertuples(index=False):
+        with open(f"/Users/{os.getlogin()}/programs/tinyserver/server/logs/sftp_log.txt", 'a') as file:
+            file.write(f"{sftp_ip(ip, dev_name)} at {datetime.now()}\n")
 
